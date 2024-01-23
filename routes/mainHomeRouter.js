ed026e199f350a038e1f5a7cb5600aaa7ae5ff0d
Abdullah Al Mahmud <abdullahalmahmud325@gmail.com>
2024-01-24 00:43:01 +0600

    showed advanced query results

--- routes/mainHomeRouter.js
+++ routes/mainHomeRouter.js
@@ -23,17 +23,23 @@ const mainHomeRouter = express.Router();
 
 mainHomeRouter
     .route('/')
-    .get( (req,res) => 
+    .get( async (req,res) => 
     {
-        res.render('index',{});
+        const sql="SELECT TYPE_NAME,CAR_TYPE_URL FROM CARTYPE";
+        const sql2="SELECT NAME FROM USERS WHERE USER_TYPE = 'CO'";
+        const car_types=await execute(sql,{});
+        console.log(car_types);
+        const company_names=await execute(sql2,{});
+        console.log(company_names);
+        res.render('index',{car_types: car_types,company_names: company_names,result: {}});
     })
     .post( async (req, res) => {
         res.write("Post is sent");
     })
     
 mainHomeRouter
-    .route('/:companyName')
-    .post( async ( req, res ) => {
+    .route('/showBrandWise/:companyName')
+    .get( async ( req, res ) => {
         console.log("asche");
         const {
             companyName
@@ -43,7 +49,11 @@ mainHomeRouter
         test();
         const result = await searchByCompany( companyName );
         console.log( result );
-        res.send("ok");
+        const sql="SELECT TYPE_NAME,CAR_TYPE_URL FROM CARTYPE";
+        const sql2="SELECT NAME FROM USERS WHERE USER_TYPE = 'CO'";
+        const car_types=await execute(sql,{});
+        const company_names=await execute(sql2,{});
+        res.render('index',{car_types: car_types,company_names: company_names,result: result});
     })
 
     module.exports = mainHomeRouter; 
